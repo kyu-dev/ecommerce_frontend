55c3fb94243f8841f135aaef5c6421eecd615c23
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-01 08:37:10 +0200

    Amélioration du composant ExperienceSection avec l'ajout d'une nouvelle image, mise à jour du texte et du style, ainsi que l'intégration d'un bouton "More infos" pour enrichir l'interaction utilisateur.

--- app/components/experience/ExperienceSection.tsx
+++ app/components/experience/ExperienceSection.tsx
@@ -1,8 +1,9 @@
+import { MoveRight } from "lucide-react";
 import React from "react";
 
 const ExperienceSection = () => {
   return (
-    <div className="flex flex-col h-screen bg-white lg:flex-row items-center justify-between gap-8 ">
+    <div className="flex flex-col h-screen bg-white lg:flex-row items-center justify-between gap-8 w-full">
       {/* Colonne image */}
       <div className="relative w-full max-w-2xl">
         {/* Rectangle arrière */}
@@ -10,21 +11,34 @@ const ExperienceSection = () => {
 
         {/* Rectangle milieu */}
         <div className="absolute top-12 -right-7 w-full h-[70%] bg-gray-100 rounded-2xl z-0"></div>
+        <img
+          src="/exp.png"
+          alt="Salon"
+          className="absolute z-9s opacity-60 blur-lg top-7 rounded-2xl right-4 shadow-xl w-full  object-cover"
+        />
 
         {/* Image principale */}
         <img
           src="/exp.png"
           alt="Salon"
-          className="relative z-10 rounded-2xl right-4 shadow-xl w-full  object-cover"
+          className="relative z-10 rounded-2xl right-4 shadow-xl w-full object-cover"
         />
       </div>
 
       {/* Colonne texte */}
-      <div className="w-full max-w-xl mt-8 lg:mt-0 text-center lg:text-left">
-        <h2 className="text-2xl font-bold mb-4">Titre ici</h2>
-        <p className="text-gray-600 mb-4">zefazefazef</p>
-        <button className="bg-black text-white px-6 py-2 rounded-full">
-          Call to action
+      <div className="w-full lg:w-1/2 ml-30 mt-8 lg:mt-0 gap-3 flex flex-col  ">
+        <h3 className=" text-orange-400 uppercase ">experiences</h3>
+        <h4 className="text-4xl">
+          we provide you the <br />
+          best experience
+        </h4>
+        <p className="text-gray-600 mb-4 max-w-md">
+          You don't have to worry about the result because all of these
+          interiors are made by people who are professionals in their fields
+          with an elegant and lucurious style and with premium quality materials
+        </p>
+        <button className="flex gap-6 text-orange-400">
+          More infos <MoveRight />
         </button>
       </div>
     </div>
