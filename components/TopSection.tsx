26f3a6be40d17765b672ea21f22b4f068757ea29
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-05 11:55:38 +0200

    Mise à jour de la page produit pour récupérer les données depuis un serveur externe et modification de l'affichage des informations produit. Changement de l'image par un message d'indisponibilité et mise à jour des propriétés de produit dans le composant TopSection.

--- components/TopSection.tsx
+++ components/TopSection.tsx
@@ -1,26 +1,24 @@
-"use client";
 import React from "react";
 import ProductCard from "./ProductCard";
 import Link from "next/link";
-import { products } from "@/data/products";
 
-const TopSection = () => {
+const TopSection = async () => {
+  const res = await fetch("http://localhost:3001/product/get");
+  const result = await res.json();
+  const products = result.data;
   return (
     <div>
       <h3 className="p-6 text-5xl font-bold">Top Produit</h3>
       <div className="p-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  xl:grid-cols-4 gap-6">
-          {products.map((product, index) => (
+          {products.map((product: any, index: number) => (
             <Link key={index} href={`/product/${product.id}`}>
               <div>
                 <ProductCard
-                  title={product.title}
+                  title={product.name}
                   price={product.price}
-                  imageUrl={product.imageUrl}
+                  imageUrl=""
                   description={product.description}
-                  onAddToCart={() =>
-                    console.log(`${product.title} added to cart`)
-                  }
                 />
               </div>
             </Link>
