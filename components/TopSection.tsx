c4347a78b66799bbf23982210b74c5ba49167c91
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-07 11:00:09 +0200

    Modification du composant TopSection pour gérer les produits de manière sécurisée avec une vérification de nullité. Mise à jour du composant Header pour utiliser un type défini pour les catégories, ajout d'une gestion de cache lors de la récupération des données et amélioration de l'affichage des catégories avec un message par défaut en cas d'absence de données.

--- components/TopSection.tsx
+++ components/TopSection.tsx
@@ -13,7 +13,7 @@ const TopSection = async () => {
       <h3 className="p-6 text-5xl font-bold">Top Produit</h3>
       <div className="p-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  xl:grid-cols-4 gap-6">
-          {products.map((product: any, index: number) => (
+          {(products ?? []).map((product: any, index: number) => (
             <Link key={index} href={`/product/${product.id}`}>
               <div>
                 <ProductCard
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,51 +2,58 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Disc2, Search, ShoppingBag, UserRound } from "lucide-react";
 
-interface Category {
+type categoryType = {
   id: number;
   name: string;
-}
+};
 
 const Header = async () => {
-  let categories: Category[] = [];
+  let categories: categoryType[] = [];
 
   try {
-    const response = await fetch("http://localhost:3000/category/get");
+    const response = await fetch("http://localhost:3000/category/get", {
+      cache: "no-store",
+    });
+
     const data = await response.json();
+    categories = data.data;
   } catch (error) {
     console.error("Erreur:", error);
   }
 
   return (
-    <>
-      <div className=" m-4 rounded-xl border">
-        <div className="flex border-b p-4 justify-between ">
-          <div className="flex gap-3 items-center">
-            <div className="flex gap-1 items-center">
-              <Disc2 />
-              <h1 className="fon">VinylShop</h1>
-            </div>
-            <Button variant="ghost">
-              <Search />
-            </Button>
+    <div className="m-4 rounded-xl border">
+      <div className="flex border-b p-4 justify-between">
+        <div className="flex gap-3 items-center">
+          <div className="flex gap-1 items-center">
+            <Disc2 />
+            <h1 className="fon">BeerShop</h1>
           </div>
-
-          <div>
-            <Button variant="ghost">
-              <ShoppingBag />
-            </Button>
-            <Button variant="ghost">
-              <UserRound />
-            </Button>
-          </div>
+          <Button variant="ghost">
+            <Search />
+          </Button>
         </div>
-        <div className="p-4 flex flex-wrap gap-6">
-          {categories.map((category: Category) => (
-            <p key={category.id}>{category.name}</p>
-          ))}
+
+        <div>
+          <Button variant="ghost">
+            <ShoppingBag />
+          </Button>
+          <Button variant="ghost">
+            <UserRound />
+          </Button>
         </div>
       </div>
-    </>
+
+      <div className="p-4 flex flex-wrap gap-6">
+        {categories.length > 0 ? (
+          categories.map((category) => (
+            <div key={category.id}>{category.name}</div>
+          ))
+        ) : (
+          <p className="text-muted-foreground">Aucune catégorie disponible.</p>
+        )}
+      </div>
+    </div>
   );
 };
 
