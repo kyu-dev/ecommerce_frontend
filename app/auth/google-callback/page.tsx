04415ea3fe303b313a72e77ad75eed06ed325318
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-09 11:21:03 +0200

    Ajout de la dépendance @tanstack/react-query pour la gestion des requêtes asynchrones. Création d'un provider ReactQueryProvider pour encapsuler l'application. Refactorisation du composant LoginBtn pour utiliser le hook useUser et suppression du store d'authentification avec Zustand. Mise à jour de la page GoogleCallback pour simplifier la redirection après connexion.

--- app/auth/google-callback/page.tsx
+++ app/auth/google-callback/page.tsx
@@ -1,20 +1,13 @@
 "use client";
 import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
 
 export default function GoogleCallback() {
-  const setToken = useAuthStore((state) => state.setToken);
   const router = useRouter();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    const token = searchParams.get("token");
-    if (token) {
-      setToken(token);
-      router.push("/"); // Redirige vers la page d'accueil ou dashboard
-    }
-  }, [setToken, router, searchParams]);
+    router.push("/");
+  }, [router]);
 
   return <div>Connexion en cours...</div>;
 }
--- components/LoginBtn.tsx
+++ components/LoginBtn.tsx
@@ -2,20 +2,25 @@
 import React from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { useAuthStore } from "@/store/authStore";
+import { useUser } from "../app/hooks/useUser";
 
 const LoginBtn = () => {
-  const token = useAuthStore((state) => state.token);
+  const { data: user, isLoading, isError } = useUser();
 
-  return (
-    <>
-      {!token && (
-        <Link href="/auth/login">
-          <Button>Login</Button>
-        </Link>
-      )}
-    </>
-  );
+  // Affiche rien pendant le chargement
+  if (isLoading) return null;
+
+  // Si l'utilisateur n'est PAS connecté (erreur ou pas de user), on affiche le bouton
+  if (isError || !user) {
+    return (
+      <Link href="/auth/login">
+        <Button>Login</Button>
+      </Link>
+    );
+  }
+
+  // Si l'utilisateur est connecté, on n'affiche rien
+  return null;
 };
 
 export default LoginBtn;
