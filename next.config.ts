a9dda8dde9a682af36e2db2c7d5c6af10f489777
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-11 14:14:30 +0200

    Simplify next.config.ts and remove custom loader

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     // Désactiver complètement l'optimisation d'images
     unoptimized: true,
-    // Loader personnalisé pour forcer les URLs directes
-    loader: 'custom',
-    loaderFile: './lib/imageLoader.js',
-    // Domaines autorisés pour les images externes
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-    ],
   },
 };
 
